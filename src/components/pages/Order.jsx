40f487e4a3477a4254af301238044142a8aec817
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-07-15 21:37:47 +0530

    Update Order.jsx

--- src/components/pages/Order.jsx
+++ src/components/pages/Order.jsx
@@ -13,7 +13,7 @@ const Order = () => {
         data.changeStatus(o._id, e.target.checked);
         data.getorders()
         //e.target.checked = e.target.checked
-        e.target.checked = data.orders.find(order => order._id === o._id).status
+        e.target.checked = JSON.parse(data.orders.find(order => order._id === o._id).status)
         console.log(JSON.parse(data.orders.find(order => order._id === o._id).status));
     }
 
