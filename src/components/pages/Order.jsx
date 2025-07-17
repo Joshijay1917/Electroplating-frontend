020dea2f89b534511eec961d12b371d1c8c1c38a
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-07-17 20:42:51 +0530

    Update Order.jsx

--- src/components/pages/Order.jsx
+++ src/components/pages/Order.jsx
@@ -34,27 +34,57 @@ const Order = () => {
             </div>
 
             {data.orders.length !== 0
-                ? <div className='mx-5 my-8 shadow-2xl border border-gray-400 rounded-2xl'>
-                    <div className='flex text-lg dark:text-white items-center justify-between w-[93%] mx-auto'>
+                ? <div className='mx-5 my-8 shadow-2xl rounded-2xl'>
+                    {/* <div className='flex text-lg dark:text-white items-center justify-between w-[93%] mx-auto'>
                         <span>Status</span>
                         <div className='flex p-3 justify-center w-[80%] '>
                             <span className='w-[50%]'>Customer</span>
                             <span>ItemName</span>
                         </div>
                         <span>Action</span>
-                    </div>
+                    </div> */}
 
-                    {data.orders.map(o => {
+                    {data.customers.length !== 0
+                          ? data.customers.map(c => {
+                            return <div key={c._id} className='mx-2 flex flex-col my-3 shadow-2xl border border-gray-400 rounded-2xl p-3'>
+                            <div className='flex items-center'>
+                                <FaUser className="text-lg text-blue-600 dark:text-blue-400 mr-3" />
+                                <h2 className="text-lg font-medium dark:text-gray-300">{c.name}</h2>
+                            </div>
+                            {/* <hr className='my-3 border border-blue-400'/> */}
+                            {data.orders.filter(o => o.customerid === c._id).map(o => {
+                                return <div className={`rounded-xl flex justify-between items-center px-5 mt-3 relative overflow-hidden shadow-gray-500 transition-all hover:-translate-y-0.5 hover:shadow-lg dark:text-white dark:bg-gray-700 dark:border-gray-600 bg-gray-50 border-gray-400 border`}>
+                                <div className="absolute top-0 left-0 w-1 h-full bg-gradient-to-b from-blue-600 to-blue-800 dark:from-blue-500 dark:to-blue-700"></div>
+                                <input type="checkbox" checked={JSON.parse(o.status)} onChange={(e) => changeStatus(e, o)} />
+                                <Link className='flex p-3 justify-center w-[80%]' to={`/orderdetails/${o._id}`}>
+                                    {/* <span className='name w-[50%] overflow-clip'>{o.customer}</span> */}
+                                    <span className='itemName overflow-clip'>{o.itemName}</span>
+                                </Link>
+                                <MdDelete onClick={e => data.deleteCustomer(o._id, "order")} className='text-2xl text-blue-600 dark:text-blue-400' />
+                            </div>
+                            })}
+                        </div>
+                          })
+                          :<div className='flex justify-center items-center'>
+                          <BiUser className="text-6xl text-blue-600 dark:text-blue-400 mr-3"/>
+                          <div>
+                          <h1 className='text-2xl font-bold dark:text-white'>No Customers</h1>
+                          <button onClick={()=>toggleForm()} className='flex bg-blue-500 text-white rounded-2xl p-1 px-3'>Add Customer <FaPlus className="text-2xl p-1 rounded-sm bg-blue-500 text-white dark:text-white mr-3"/></button>
+                          </div>
+                        </div>
+                        }
+
+                    {/* {data.orders.map(o => {
                         return <div key={o._id} className={`rounded-xl flex justify-between items-center px-8 mx-3 my-3 relative overflow-hidden shadow-gray-500 transition-all hover:-translate-y-0.5 hover:shadow-lg dark:text-white dark:bg-gray-700 dark:border-gray-600 bg-gray-50 border-gray-400 border`}>
                                 <div className="absolute top-0 left-0 w-1 h-full bg-gradient-to-b from-blue-600 to-blue-800 dark:from-blue-500 dark:to-blue-700"></div>
-                                <input type="checkbox" checked={JSON.parse(o.status)} onChange={(e)=>changeStatus(e, o)} />
+                                <input type="checkbox" checked={JSON.parse(o.status)} onChange={(e)=>changeStatus(e)} />
                                 <Link className='flex p-3 justify-center w-[80%]' to={`/orderdetails/${o._id}`}>
                                     <span className='name w-[50%] overflow-clip'>{o.customer}</span>
                                     <span className='itemName overflow-clip'>{o.itemName}</span>
                                 </Link>
                                 <MdDelete onClick={e=>data.deleteCustomer(o._id, "order")} className='text-2xl text-blue-600 dark:text-blue-400' />
                             </div>
-                    })}
+                    })} */}
                 </div>
 
                 : <div className='flex justify-center items-center m-8'>
