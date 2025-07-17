13b46c45621c54ff014b5268179122b63f950e53
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-07-17 21:51:22 +0530

    Update Order.jsx

--- src/components/pages/Order.jsx
+++ src/components/pages/Order.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { FaClipboardList, FaPlus, FaUser } from 'react-icons/fa'
 import "../setheight.css"
 import { MdDelete } from 'react-icons/md'
@@ -8,6 +8,8 @@ import { BiUser } from 'react-icons/bi'
 
 const Order = () => {
     const data = useContext(Store)
+    const [currentOrder, setcurrentOrder] = useState([])
+    const [currentCus, setcurrentCus] = useState([])
 
     const changeStatus = (e, o) => {
         data.changeStatus(o._id, e.target.checked);
@@ -19,6 +21,16 @@ const Order = () => {
 
     useEffect(() => {
         data.setcurrentPage("Order")
+        const getOrders = async() => {
+            setcurrentOrder(data.orders.filter(o =>
+                data.customers.find(c => c._id === o.customerid)
+            ));
+            setcurrentCus(data.customers.filter(c => 
+                data.orders.find(o => o.customerid === c._id)
+            ))
+        }
+        getOrders();
+        
     }, [])
 
     return (
@@ -33,7 +45,7 @@ const Order = () => {
                 </Link>
             </div>
 
-            {data.orders.length !== 0
+            {currentOrder.length !== 0
                 ? <div className='mx-5 my-8 shadow-2xl rounded-2xl'>
                     {/* <div className='flex text-lg dark:text-white items-center justify-between w-[93%] mx-auto'>
                         <span>Status</span>
@@ -44,15 +56,15 @@ const Order = () => {
                         <span>Action</span>
                     </div> */}
 
-                    {data.customers.length !== 0
-                          ? data.customers.map(c => {
+                    {currentCus.length !== 0
+                          ? currentCus.map(c => {
                             return <div key={c._id} className='mx-2 flex flex-col my-3 shadow-2xl border border-gray-400 rounded-2xl p-3'>
                             <div className='flex items-center'>
                                 <FaUser className="text-lg text-blue-600 dark:text-blue-400 mr-3" />
                                 <h2 className="text-lg font-medium dark:text-gray-300">{c.name}</h2>
                             </div>
                             {/* <hr className='my-3 border border-blue-400'/> */}
-                            {data.orders.filter(o => o.customerid === c._id).map(o => {
+                            {currentOrder.map(o => {
                                 return <div className={`rounded-xl flex justify-between items-center px-5 mt-3 relative overflow-hidden shadow-gray-500 transition-all hover:-translate-y-0.5 hover:shadow-lg dark:text-white dark:bg-gray-700 dark:border-gray-600 bg-gray-50 border-gray-400 border`}>
                                 <div className="absolute top-0 left-0 w-1 h-full bg-gradient-to-b from-blue-600 to-blue-800 dark:from-blue-500 dark:to-blue-700"></div>
                                 <input type="checkbox" checked={JSON.parse(o.status)} onChange={(e) => changeStatus(e, o)} />
