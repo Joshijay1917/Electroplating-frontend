28714a32080f3d1f5570f8223a56f68a7272ad1b
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-07-18 20:21:00 +0530

    Update CustomerForm.jsx

--- src/components/Forms/CustomerForm.jsx
+++ src/components/Forms/CustomerForm.jsx
@@ -3,6 +3,7 @@ import { Store } from '../../Context/Store'
 
 const CustomerForm = ({ toggleForm }) => {
     const store = useContext(Store)
+    const [loading, setloading] = useState(0)
     const [FormData, setFormData] = useState({
         "name": '',
         "phone": ''
@@ -37,21 +38,26 @@ const CustomerForm = ({ toggleForm }) => {
     }
 
     return (
-        <div className='bg-black/30 absolute w-full h-full top-0 z-10'>
-            <div className='h-full flex items-center flex-col justify-center'>
-            <form onSubmit={handlesubmit} className='flex p-6 rounded-2xl items-start gap-3 flex-col justify-center bg-white'>
-                <h1 className='text-2xl font-bold mb-6'>Customer Form</h1>
-                <div className='flex gap-3 mx-auto items-center'>
-                    <label>Name</label>
-                    <input onChange={handleChange} className='bg-gray-300 rounded-2xl p-2' type="text" name='name' placeholder='Customer Name' />
+        <div className='w-full h-full fixed z-30 top-0 justify-center items-center'>
+            <div className='bg-black/30 absolute w-full h-full top-0'>
+                <div className='h-full flex items-center flex-col justify-center'>
+                    <form onSubmit={handlesubmit} className='flex p-6 rounded-2xl items-start gap-3 z-30 flex-col justify-center bg-white'>
+                        <h1 className='text-2xl font-bold mb-6'>Customer Form</h1>
+                        <div className='flex gap-3 mx-auto items-center'>
+                            <label>Name</label>
+                            <input onChange={handleChange} className='bg-gray-300 rounded-2xl p-2' type="text" name='name' placeholder='Customer Name' />
+                        </div>
+                        <div className='flex gap-3 mx-auto items-center'>
+                            <label className='phone'>Phone</label>
+                            <input onChange={handleChange} className='bg-gray-300 rounded-2xl p-2' type="text" name='phone' placeholder='Phone Number' />
+                        </div>
+                        <button className='mt-3 bg-blue-500 text-white px-3 py-2 rounded-2xl' type='submit'>Add Customer</button>
+                    </form>
                 </div>
-                <div className='flex gap-3 mx-auto items-center'>
-                    <label className='phone'>Phone</label>
-                    <input onChange={handleChange} className='bg-gray-300 rounded-2xl p-2' type="text" name='phone' placeholder='Phone Number' />
-                </div>
-                <button className='mt-3 bg-blue-500 text-white px-3 py-2 rounded-2xl' type='submit'>Add Customer</button>
-            </form>
             </div>
+            {loading ? <div className="bg-black/30 fixed z-30 w-full h-full flex justify-center items-center">
+                <div className="animate-spin rounded-full border-4 border-solid border-t-transparent text-blue-800 h-19 w-19"></div>
+            </div> : null}
         </div>
     )
 }
