343fb05973d65b19036c232af75c47d50c03f540
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-07-19 20:42:26 +0530

    Update StasCard.jsx

--- src/components/Content/StasCard.jsx
+++ src/components/Content/StasCard.jsx
@@ -1,4 +1,11 @@
-export default function StatsCard({ value, label, bg }) {
+import { useContext, useEffect } from "react"
+import { Store } from "../../Context/Store"
+
+export default function StatsCard({ value, label, bg, page }) {
+    const data = useContext(Store)
+  useEffect(() => {
+    data.setcurrentPage(page)
+  }, [])
     return (
       <div className={`rounded-xl p-6 text-center transition-all hover:-translate-y-1 hover:shadow-lg dark:border-gray-600 ${bg} border-gray-400 border`}>
         <div className="text-3xl font-bold text-white dark:text-gray-100 mb-2">{value}</div>
