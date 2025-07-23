5bcf9930ecf080d58ffb13e457d14ea205173601
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-07-23 17:50:18 +0530

    Update Order.jsx

--- src/components/pages/Order.jsx
+++ src/components/pages/Order.jsx
@@ -37,7 +37,7 @@ const Order = () => {
 
     return (
         <div className='setheight pb-[68px]'>
-            <div className='mx-5 flex items-center justify-between my-8 shadow-2xl border border-gray-400 rounded-2xl p-4'>
+            <div className='mx-5 flex items-center justify-between my-8 shadow-2xl border text-gray-700 dark:text-white border-gray-400 rounded-2xl p-4'>
                 <div className='flex items-center'>
                     <FaClipboardList className="text-xl text-blue-600 dark:text-blue-400 mr-3" />
                     <h2 className="text-xl font-semibold">All Orders</h2>
@@ -60,14 +60,14 @@ const Order = () => {
 
                     {currentCus.length !== 0
                           ? currentCus.map(c => {
-                            return <div key={c._id} className='mx-2 flex flex-col my-3 shadow-2xl border border-gray-400 rounded-2xl p-3'>
+                            return <div key={c._id} className='mx-2 flex flex-col my-3 shadow-2xl border text-gray-700 dark:text-white border-gray-400 rounded-2xl p-3'>
                             <div className='flex items-center'>
                                 <FaUser className="text-lg text-blue-600 dark:text-blue-400 mr-3" />
                                 <h2 className="text-lg font-medium">{c.name}</h2>
                             </div>
                             {/* <hr className='my-3 border border-blue-400'/> */}
                             {currentOrder.filter(o => o.customerid === c._id).map(o => {
-                                return <div className={`rounded-xl flex justify-between items-center px-5 mt-3 relative overflow-hidden shadow-gray-500 transition-all hover:-translate-y-0.5 hover:shadow-lg dark:bg-gray-700 dark:border-gray-600 bg-gray-50 border-gray-400 border`}>
+                                return <div className={`rounded-xl flex justify-between items-center px-5 mt-3 relative overflow-hidden text-gray-700 dark:text-white shadow-gray-500 transition-all hover:-translate-y-0.5 hover:shadow-lg dark:bg-gray-700 dark:border-gray-600 bg-gray-50 border-gray-400 border`}>
                                 <div className="absolute top-0 left-0 w-1 h-full bg-gradient-to-b from-blue-600 to-blue-800 dark:from-blue-500 dark:to-blue-700"></div>
                                 <input type="checkbox" checked={JSON.parse(o.status)} onChange={(e) => changeStatus(e, o)} />
                                 <Link className='flex p-3 justify-center w-[80%]' to={`/orderdetails/${o._id}`}>
@@ -79,7 +79,7 @@ const Order = () => {
                             })}
                         </div>
                           })
-                          :<div className='flex justify-center items-center'>
+                          :<div className='flex justify-center text-gray-700 dark:text-white items-center'>
                           <BiUser className="text-6xl text-blue-600 dark:text-blue-400 mr-3"/>
                           <div>
                           <h1 className='text-2xl font-bold dark:text-white'>No Customers</h1>
@@ -101,7 +101,7 @@ const Order = () => {
                     })} */}
                 </div>
 
-                : <div className='flex justify-center items-center m-8'>
+                : <div className='flex justify-center text-gray-700 dark:text-white items-center m-8'>
                     <BiUser className="text-6xl text-blue-600 dark:text-blue-400 mr-3" />
                     <div>
                         <h1 className='text-2xl font-bold'>No Orders</h1>
