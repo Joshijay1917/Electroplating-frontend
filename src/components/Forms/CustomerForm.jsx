52860859796eed5e1c9a0244a385d84229e390b9
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-07-24 21:28:06 +0530

    Update CustomerForm.jsx

--- src/components/Forms/CustomerForm.jsx
+++ src/components/Forms/CustomerForm.jsx
@@ -48,22 +48,22 @@ const CustomerForm = ({ toggleForm }) => {
         <div className='w-full h-full fixed z-30 top-0 justify-center items-center'>
             <div className='bg-black/30 absolute w-full h-full top-0'>
                 <div className='h-full flex items-center flex-col justify-center'>
-                    <form onSubmit={handlesubmit} className='flex p-6 rounded-2xl items-start gap-3 z-30 flex-col justify-center bg-white'>
-                        <div className='flex justify-between w-full items-center text-2xl font-bold'>
+                    <form onSubmit={handlesubmit} className='flex p-6 rounded-2xl items-start w-[80%] overflow-clip gap-3 z-30 flex-col justify-center bg-white'>
+                        <div className='flex justify-between w-full items-center text-xl font-bold'>
                             <div className='flex items-center'>
                                 <FaUser className="text-2xl text-blue-600 dark:text-blue-400 mr-3"/>
                                 <h1 className='text-gray-700 dark:text-white'>Customer Form</h1>
                             </div>
-                            <GiCancel onClick={() => navigate('/customer')} className='text-3xl text-blue-600 dark:text-blue-400'/>
+                            <GiCancel onClick={() => toggleForm()} className='text-3xl text-blue-600 dark:text-blue-400'/>
                         </div>
                         <hr className='mb-4 border border-blue-400 w-full' />
                         <div className='flex gap-3 mx-auto items-center'>
                             <label>Name</label>
-                            <input onChange={handleChange} className='bg-gray-300 rounded-2xl p-2' type="text" name='name' placeholder='Customer Name' />
+                            <input onChange={handleChange} className='bg-gray-300 w-full rounded-2xl p-2' type="text" name='name' placeholder='Customer Name' />
                         </div>
                         <div className='flex gap-3 mx-auto items-center'>
                             <label className='phone'>Phone</label>
-                            <input onChange={handleChange} className='bg-gray-300 rounded-2xl p-2' type="text" name='phone' placeholder='Phone Number' />
+                            <input onChange={handleChange} className='bg-gray-300 w-full rounded-2xl p-2' type="text" name='phone' placeholder='Phone Number' />
                         </div>
                         <button className='mt-3 bg-blue-500 text-white px-3 py-2 rounded-2xl' type='submit'>Add Customer</button>
                     </form>
