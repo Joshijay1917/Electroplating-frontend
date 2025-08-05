7cd4b4f1bf1e32d48c0944c1007af598c269d987
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-08-05 21:11:38 +0530

    Update Login.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,40 +1,58 @@
-import React, {useState} from 'react'
+import React, { useContext, useState } from 'react'
 import './Login.css'
+import { Store } from '../../Context/Store'
 
-const Login = () => {
-    const [anim, setanim] = useState(true)
-    const [form, setform] = useState(true)
+const Login = ({Animation, revAnimation, setisAuthenticated}) => {
+    const data = useContext(Store)
+    const [formData, setformData] = useState({
+        username: "",
+        password: ""
+    })
 
-    const animfun = () => {
-        setanim(false);
-        setTimeout(() => {
-            setform(false);
-        }, 3000);
+    const handleInput = (e) => {
+        setformData(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const Login = async () => {
+        if(!formData.username || !formData.password) {
+            data.showNotification("Username and Password is required!", "error");
+            return;
+        }
+
+        Animation();
+
+        const data2 = await fetch('http://localhost:3000/api/login', {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        const res = await data2.json();
+
+        if(res.status === 400) {
+            revAnimation()
+            data.showNotification(res.msg, "error")
+        } else {
+            setisAuthenticated(true)
+        }
     }
 
     return (
-        <div className='dark:bg-gray-700 bg-linear-to-r from-sky-500 to-blue-400 w-[100vw] h-[100vh]'>
-            <div className='flex justify-center items-center text-white dark:text-white'>
-                <img className='w-[70px] md:w-[82px] h-[70px] md:h-[82px] rounded-full' src="logo2nobg.png" alt="" />
-                <div className='px-2 py-4'>
-                    <h1 className='text-[21px] font-bold md:text-4xl'> Harshd Electroplating</h1>
-                    <p className='text-[18px] font-bold md:text-xl'>Order Management</p>
-                </div>
+        <div className='mt-[7vh] snap-center gap-6 flex flex-col justify-center items-center'>
+            <h1 className='font-bold text-2xl  underline underline-offset-10 decoration-7 decoration-blue-400'>Login</h1>
+            <div className='flex bg-gray-300 p-2 rounded-2xl w-[85%] gap-3'>
+                <label>Username:</label>
+                <input onChange={handleInput} className='outline-0' type="text" name='username' placeholder='username' />
             </div>
-            {form && <div className={`${anim ? 'login' : 'login2'} h-[80vh] bg-gray-200 dark:bg-gray-500 absolute overflow-clip w-full rounded-t-4xl top-[20vh]`}>
-                <div className='w-full h-[70%] gap-4 flex flex-col justify-center items-center text-gray-800 dark:text-white'>
-                    <h1 className='font-bold text-2xl  underline underline-offset-10 decoration-7 decoration-blue-400 m-8'>Login</h1>
-                    <div className='flex gap-2 items-center'>
-                        <label>Username:</label>
-                        <input type="text" className='bg-gray-300 p-2 rounded-2xl' />
-                    </div>
-                    <div className='flex gap-2 items-center'>
-                        <label>Password:</label>
-                        <input type="text" className='bg-gray-300 p-2 rounded-2xl' />
-                    </div>
-                    <button onClick={()=>animfun()} className='bg-blue-400 text-white px-4 m-4 py-2 rounded-2xl'>Login</button>
-                </div>
-            </div>}
+            <div className='flex bg-gray-300 p-2 rounded-2xl w-[85%] gap-3'>
+                <label>Password:</label>
+                <input onChange={handleInput} className='outline-0' type="password" name='password' placeholder='password' />
+            </div>
+            <button onClick={() => Login()} className='text-white bg-blue-400 px-3 py-2 rounded-2xl'>Login</button>
         </div>
     )
 }
