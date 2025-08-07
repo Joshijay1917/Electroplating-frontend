cd93d55c405efb7a2d920bb8a5942d8e367672d3
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-08-07 12:36:01 +0530

    Update Main.jsx

--- src/components/Auth/Main.jsx
+++ src/components/Auth/Main.jsx
@@ -26,7 +26,7 @@ const Main = ({setisAuthenticated}) => {
         <div className='dark:bg-gray-700 bg-linear-to-r from-sky-500 to-blue-400 w-[100vw] h-[100vh]'>
             <div className={`${goDown ? "goDown" : "goUp"} ${setThings ? "h-0 top-[100%]" : "h-[80%] top-[20%]"} bg-white relative`}></div>
                 <img className={`${goDown ? "logo animate-spin" : ""} ${setThings ? "settop" : ""} z-10 absolute top-[15%] setleft bg-white rounded-full`} width={80} height={80} src="./logo2nobg.png" alt="" />
-                <div className={`${goDown ? "form" : ""} ${setThings ? "h-[1vh] p-[15px] top-[50%]" : "h-[70%] top-[20%]"} overflow-clip bg-black/10 p-6 absolute left-[5%] rounded-2xl w-[90%] mx-auto flex flex-col justify-center items-center`}>
+                <div className={`${goDown ? "form" : "formUp"} ${setThings ? "h-[1vh] p-[15px] top-[50%]" : "h-[70%] top-[20%]"} overflow-clip bg-black/10 p-6 absolute left-[5%] rounded-2xl w-[90%] mx-auto flex flex-col justify-center items-center`}>
                 <div className='snap-x snap-mandatory'>
                     <Login Animation={Animation} revAnimation={revAnimation} setisAuthenticated={setisAuthenticated}/>
                     {/* <Register Animation={Animation} revAnimation={revAnimation}/> */}
